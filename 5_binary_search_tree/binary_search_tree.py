4587a99b0a5e205746be4d687cebe1fc68af54c0
ebisLab <ebaiden@techie.com>
2020-05-14 18:49:49 -0400

    binary day 1

--- 5_binary_search_tree/binary_search_tree.py
+++ 5_binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 """
-Binary search trees are a data structure that enforce an ordering over 
-the data they store. That ordering in turn makes it a lot more efficient 
-at searching for a particular piece of data in the tree. 
+Binary search trees are a data structure that enforce an ordering over
+the data they store. That ordering in turn makes it a lot more efficient
+at searching for a particular piece of data in the tree.
 
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
@@ -9,6 +9,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,20 +19,66 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # checking if the incoming value is less than the current node value
+        if value < self.value:  # we're comparing in insert to know which direction to go in
+            # go left
+            # signal to recurse again
+            # or when to stop
+            if not self.left:
+                # we can park our value here
+                self.left = BSTNode(value)
+            else:
+                # we cant park here
+                # we still ned to go left, but we need to still keep looking
+                self.left.insert(value)
+        else:
+            # go right
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)  # no need to return anything
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # When we start searching, self will be the root
+        # compare the target against self
+        #
+        # criteria for returning fals: we know we need o go in one direction
+        # but there's no children
+        if target == self.value:  # we're comparing to stop our recursion. We might find our value right off the bat
+            return True
+        if target < self.value:
+            # go left if left is a BSTnode
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            # go right
+            if not self.right:
+                return False
+            return self.right.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # we'll
+        if not self.right:
+            return self.value
+        # otherswise, keep giong righ
+        return self.right.get_max()
+        return None  # why not return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # call the fn on the value at this node
+        fn(self.value)
+
+        # pass this function to the left child
+        if self.left:
+            self.left.for_each(fn)
+        # pass this function to the right child
+        if self.right:
+            self.right.for_each(fn)
 
     # Part 2 -----------------------
 
